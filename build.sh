24e3853a94ff8c1876562dabad1e7ff84ceba1fb
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-27 11:44:16 +0100

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
+#!/bin/bash
 docker build --no-cache -t hackinglab/alpine-tinyproxy:3.2.0 -t hackinglab/alpine-tinyproxy:3.2 -t hackinglab/alpine-tinyproxy:latest -f Dockerfile .
 
-docker push hackinglab/alpine-tinyproxy
-docker push hackinglab/alpine-tinyproxy:3.2
-docker push hackinglab/alpine-tinyproxy:3.2.0
